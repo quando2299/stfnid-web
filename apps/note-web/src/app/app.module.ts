4dadcbf5658b2ef7fd0ae0cd2b726357d24be2b3
quando2299 <quando.2299@gmail.com>
2023-04-17 03:11:42 +0700

    Initial, Update Repo

---
+++ apps/note-web/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { SharedModule } from 'stfnid-workspace/shared';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ libs/shared/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/shared.module';
+
+export * from './lib/loading/loading.component';
